2f05eb0ffb84104dd9a9b61211b95c3c469bcc12
gaoleijie <gaoleijie@kingxunlian.com>
2019-04-10 22:01:33 +0800

    ‘submit’

--- api-gateway/src/main/java/com/glj/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/glj/gateway/ApiGatewayApplication.java
@@ -1,15 +1,17 @@
-package com.glj;
+package com.glj.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class AuthenticationCenterApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthenticationCenterApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
